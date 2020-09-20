f73d04f6238afda9b9c69441a5ef9f0c5d47177d
Никита Левашов <sh4rpy@ya.ru>
2020-09-20 19:23:59 +0300

    added a new command /today, added a default valid value for a failed request (get_html), added the ability to select a date range

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import re
+from datetime import datetime
 
 import requests
 import telebot
@@ -12,20 +13,27 @@ load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 EDUCATION_PROGRAM_URL = 'http://znanie.vdnh.ru/?dates={}'
-RE_DATE = r'^20[0-2][0-9].((0[1-9])|(1[0-2])).([0-2][1-9]|3[0-1])$'
+RE_DATE = r'^20[0-2][0-9].((0[1-9])|(1[0-2])).(0[1-9]|[1-2][0-9]|3[0-1])$'
+RE_DATES_RANGE = r'^20[0-2][0-9].((0[1-9])|(1[0-2])).(0[1-9]|[1-2][0-9]|3[0-1])-' \
+                 r'20[0-2][0-9].((0[1-9])|(1[0-2])).(0[1-9]|[1-2][0-9]|3[0-1])$'
 
 
 def get_html(date):
-    """Возвращает контент html-страницы с get-параметром date"""
+    """Возвращает контент html-страницы с get-параметром dates"""
+    url = EDUCATION_PROGRAM_URL.format(date)
     try:
-        response = requests.get(EDUCATION_PROGRAM_URL.format(date), timeout=30)
-    except requests.Timeout:
-        # лог для Heroku
-        print('TIMEOUT ERROR')
-    else:
+        response = requests.get(url, timeout=30)
         # лог для Heroku
         print('The response is received successfully')
         return response.content
+    except requests.Timeout:
+        # лог для Heroku
+        print('TIMEOUT ERROR')
+    except requests.RequestException:
+        # лог для Heroku
+        print(f'The request failed at the address: {url}')
+    # возвращаем дефолтное валидное значение при неудавшемся запросе
+    return b''
 
 
 def parse_events(response):
@@ -34,6 +42,7 @@ def parse_events(response):
     answer = ''
     html = bs(response, 'html.parser')
     event_items = html.find_all('div', class_='event')
+    # выбираем нужные элементы на странице
     for event in event_items:
         events.append({
             'place': event.find('div', class_='place').text.lstrip(),
@@ -41,6 +50,7 @@ def parse_events(response):
             'date': event.find('div', class_='fulldate').find_all('div')[0].text.strip(),
             'time': event.find('div', class_='fulldate').find_all('div')[1].text.strip(),
         })
+    # проходимся по элементам
     for event in events:
         # делаем проверку на площадку, так как не все нас интересуют
         if event['place'].strip() in (
@@ -49,6 +59,7 @@ def parse_events(response):
                 'Летний кинотеатр-лекторий',
                 'Крыша павильона «Рабочий и колхозница»'
         ):
+            # и добавляем элемент в ответ
             answer += f'<b>Площадка:</b>\n{event["place"]}\n' \
                       f'<b>Описание:</b>\n{event["title"]}\n' \
                       f'<b>Дата:</b>\n{event["date"]}\n' \
@@ -56,23 +67,38 @@ def parse_events(response):
     return answer
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def greeting(message):
-    """Команды /start и /help"""
-    if message.text == '/start':
-        with open('stickers/AnimatedSticker.tgs', 'rb') as sticker:
-            bot.send_sticker(message.chat.id, sticker)
-        bot.send_message(message.chat.id, text=f'Привет, {message.from_user.first_name}!')
+    """Приветствие при команде /start"""
+    with open('stickers/AnimatedSticker.tgs', 'rb') as sticker:
+        bot.send_sticker(message.chat.id, sticker)
+    bot.send_message(message.chat.id, text=f'Привет, {message.from_user.first_name}!\n\n'
+                                           f'Узнайте, как мной пользоваться /help.')
 
-    else:
-        bot.send_message(message.chat.id,
-                         text='Отправьте дату в формате YYYY.MM.DD, чтобы увидеть мероприятия в этот день.')
+
+@bot.message_handler(commands=['help'])
+def show_help_text(message):
+    """Показывает подсказку по использованию бота при команде /help"""
+    bot.send_message(message.chat.id,
+                     text='Отправьте дату в формате YYYY.MM.DD, чтобы увидеть мероприятия в этот день.\n\n'
+                          'Отправьте диапозон дат в формате YYYY.MM.DD-YYYY.MM.DD, '
+                          'чтобы увидеть мероприятия в эти дни.\n\n'
+                          'Команда /today показывает сегодняшние мероприятия.')
+
+
+@bot.message_handler(commands=['today'])
+def show_today_events(message):
+    today = datetime.today().strftime('%Y.%m.%d')
+    try:
+        bot.send_message(message.chat.id, parse_events(get_html(today)), parse_mode='html')
+    except ApiTelegramException:
+        bot.send_message(message.chat.id, f'На {today} не нашлось мероприятий')
 
 
 @bot.message_handler(content_types=['text'])
-def show_events(message):
+def show_events_by_date(message):
     """Формирует ответ пользователю"""
-    if re.search(RE_DATE, message.text):
+    if re.search(RE_DATE, message.text) or re.search(RE_DATES_RANGE, message.text):
         try:
             bot.send_message(message.chat.id, parse_events(get_html(message.text)), parse_mode='html')
         except ApiTelegramException:
